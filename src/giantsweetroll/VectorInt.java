be6e28b889df9c42ea82353c5d2bcb61b0b7fdef
GiantSweetroll <33750894+whiteMordor@users.noreply.github.com>
2017-11-20 16:57:26 +0700

    Added the class VectorInt
    VectorInt is a class used to store x, y, z values, similar to that of the Dimension class Java. The class name is VectorInt because the x, y, z values are integers. More similar classes will be added later with different value types.

---
+++ src/giantsweetroll/VectorInt.java
@@ -0,0 +1,51 @@
+package giantsweetroll;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class VectorInt.
+ */
+public class VectorInt
+{
+	
+	/** The x. */
+	public int x;
+	
+	/** The y. */
+	public int y;
+	
+	/** The z. */
+	public int z;
+	
+	/**
+	 * Instantiates a new vector with x and y.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
+	public VectorInt(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Instantiates a new vector with no values.
+	 */
+	public VectorInt()
+	{
+	}
+	
+	/**
+	 * Instantiates a new vector with x, y, and z.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param z the z
+	 */
+	public VectorInt(int x, int y, int z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
