9fd1afc9a128eaed912635fbc449971130b720bb
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2018-06-13 15:09:56 +0700

    more updates
    - Renamed Misc class to GMisc
    - Added DoubleFilter and LongFilter class

---
+++ src/giantsweetroll/GMisc.java
@@ -0,0 +1,56 @@
+package giantsweetroll;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Enumeration;
+
+import javax.swing.JComboBox;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class GMisc.
+ */
+public class GMisc 
+{
+	
+	/**
+	 * Gets the screen size.
+	 *
+	 * @return the screen size
+	 */
+	public static Dimension getScreenSize()
+	{
+		return Toolkit.getDefaultToolkit().getScreenSize();
+	}
+	
+	/**
+	 * Gets the selected item in the JComboBox.
+	 *
+	 * @param combo the JComboBox
+	 * @return the item
+	 */
+	public static Object getItem(JComboBox combo)
+	{
+		return combo.getSelectedItem();
+	}
+	
+	public static void setUIFont(FontUIResource f)
+	{
+		Enumeration keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements())
+		{
+			Object key = keys.nextElement();
+			Object value = UIManager.get(key);
+			if (value instanceof FontUIResource)
+			{
+				UIManager.put(key, f);
+			}
+		}
+	}
+	public static void setUIFont(String fontName, int fontType, int fontSize)
+	{
+		setUIFont(new FontUIResource(fontName, fontType, fontSize));
+	}
+}
---
+++ src/giantsweetroll/filters/LongFilter.java
@@ -0,0 +1,103 @@
+package giantsweetroll.filters;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+
+public class LongFilter  extends DocumentFilter
+{
+	private long min, max;
+	
+	public LongFilter(long min, long max)
+	{
+		this.min = min;
+		this.max = max;
+	}
+	 
+	 	private boolean lessThanMin(long num)
+	 	{
+	 		return num<min;
+	 	}
+	 	private boolean moreThanMax(long num)
+	 	{
+	 		return num>max;
+	 	}
+	 
+	   private boolean isLong(String text) {
+	      try {
+	         Long.parseLong(text);
+	         return true;
+	      } catch (NumberFormatException e) {
+	         return false;
+	      }
+	   }
+
+	   //Overriden Methods
+	   @Override
+	   public void insertString(FilterBypass fb, int offset, String string,
+	         AttributeSet attr) throws BadLocationException {
+
+	      Document doc = fb.getDocument();
+	      StringBuilder sb = new StringBuilder();
+	      sb.append(doc.getText(0, doc.getLength()));
+	      sb.insert(offset, string);
+
+	      if (isLong(sb.toString())){
+	         if (this.lessThanMin(Long.parseLong(string)))
+	         {
+	        	 string = Long.toString(min);
+	         }
+	         else if (this.moreThanMax(Long.parseLong(string)))
+	         {
+	        	 string = Long.toString(max);
+	         }
+	         super.insertString(fb, offset, string, attr);
+	      } else {
+	         // warn the user and don't allow the insert
+	      }
+	   }
+	   @Override
+	   public void replace(FilterBypass fb, int offset, int length, String text,
+	         AttributeSet attrs) throws BadLocationException {
+
+	      Document doc = fb.getDocument();
+	      StringBuilder sb = new StringBuilder();
+	      sb.append(doc.getText(0, doc.getLength()));
+	      sb.replace(offset, offset + length, text);
+
+	      if (isLong(sb.toString())) 
+	      {
+		         if (this.lessThanMin(Long.parseLong(text)))
+		         {
+		        	 text = Long.toString(min);
+		         }
+		         else if (this.moreThanMax(Long.parseLong(text)))
+		         {
+		        	 text = Long.toString(max);
+		         }
+	         super.replace(fb, offset, length, text, attrs);
+	          
+	      } else {
+	         // warn the user and don't allow the insert
+	      }
+
+	   }
+
+	   @Override
+	   public void remove(FilterBypass fb, int offset, int length)
+	         throws BadLocationException {
+	      Document doc = fb.getDocument();
+	      StringBuilder sb = new StringBuilder();
+	      sb.append(doc.getText(0, doc.getLength()));
+	      sb.delete(offset, offset + length);
+
+	      if (isLong(sb.toString())) {
+	         super.remove(fb, offset, length);
+	      } else {
+	         // warn the user and don't allow the insert
+	      }
+
+	   }
+}
+
