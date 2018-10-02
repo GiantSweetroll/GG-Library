e51bd789a4a0a9940e4b731b50e148faa74aa269
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2018-10-02 14:43:04 +0700

    Addition to date
    - Added DateSelectionPanel class
    - Added more methods to DateManager class

--- src/giantsweetroll/date/DateManager.java
+++ src/giantsweetroll/date/DateManager.java
@@ -4,7 +4,11 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -12,6 +16,14 @@ import java.util.Date;
  */
 public class DateManager 
 {
+public static final byte FEBRUARY = 2;
+	
+	public static final byte[] MONTHS_31 = {1, 3, 5, 7, 8, 10, 12};
+	public static final byte[] MONTHS_30 = {2, 4, 6, 9, 11};
+	
+	public static final List<String> MONTH_NAME_LIST = getMonthNameList();
+	
+	public static final short START_YEAR_RANGE = 1800;
 	
 	private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     
@@ -122,4 +134,195 @@ public class DateManager
     	
     	return time.substring(6, 8);
     }
+    
+    public static byte[] getMaxDays(byte month, short year)
+	{
+		byte[] days;
+		
+		if (month == FEBRUARY)
+		{
+			if (isLeapYear(year))
+			{
+				days = new byte[29];
+				for (byte i=0; i<days.length; i++)
+				{
+					days[i] = (byte)(i+1);
+				}
+			}
+			else
+			{
+				days = new byte[28];
+				for (byte i=0; i<days.length; i++)
+				{
+					days[i] = (byte)(i+1);
+				}
+			}
+		}
+		else
+		{
+			if (is30Days(month))
+			{
+				days = new byte[30];
+				for (byte i=0; i<days.length; i++)
+				{
+					days[i] = (byte)(i+1);
+				}
+			}
+			else
+			{
+				days = new byte[31];
+				for (byte i=0; i<days.length; i++)
+				{
+					days[i] = (byte)(i+1);
+				}
+			}
+		}
+		
+		return days;
+	}
+	public static short[] getYearRange()
+	{
+		short startYear = START_YEAR_RANGE;
+		short maxYear = Short.parseShort(DateManager.getCurrentYear());
+		
+		short[] years;
+		
+		if (startYear == maxYear)
+		{
+			years = new short[1];
+			years[0] = startYear;
+		}
+		else
+		{
+			short range = (short)(maxYear - startYear);
+			years = new short[range+1];
+			
+			for (short i=0; i<=range; i++)
+			{
+				years[i] = (short)(startYear+i);
+			}
+		}
+		
+		return years;
+	}
+	public static String[] getYearRangeString()
+	{
+		short[] raw = getYearRange();
+		String[] string = new String[raw.length];
+		
+		for (int i=0; i<string.length; i++)
+		{
+			string[i] = Short.toString(raw[i]);
+		}
+		
+		return string;
+	}
+	public static String[] getMaxDaysString(byte month, short year)
+	{
+		byte[] days = getMaxDays(month, year);
+		String[] daysString = new String[days.length];
+		
+		for (int i=0; i<daysString.length; i++)
+		{
+			if (isOneDigit(Byte.toString(days[i])))
+			{
+				daysString[i] = 0 + Byte.toString(days[i]);
+			}
+			else
+			{
+				daysString[i] = Byte.toString(days[i]);
+			}
+		}
+		
+		return daysString;
+	}
+	public static String[] getMonthsString()
+	{
+		String[] arr = new String[12];
+		
+		for (int i=0; i<arr.length; i++)
+		{
+			if (isOneDigit(Integer.toString(i)))
+			{
+				arr[i] = 0 + Integer.toString(i+1);
+			}
+			else
+			{
+				arr[i] = Integer.toString(i+1);
+			}
+		}
+		
+		return arr;
+	}
+	public static boolean is30Days(byte month)
+	{
+		for (int i=0; i<MONTHS_30.length; i++)
+		{
+			if (month == MONTHS_30[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean is31Days(byte month)
+	{
+		for (int i=0; i<MONTHS_31.length; i++)
+		{
+			if (month == MONTHS_31[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean isLeapYear(int year)
+	{
+		if (year%4==0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static String[] getYearRangeStringReversed()
+	{
+		List<String> list = Arrays.asList(getYearRangeString());
+		Collections.reverse(list);
+		return list.toArray(new String[list.size()]);
+	}
+	
+	public static final List<String> getMonthNameList()
+	{
+		List<String> list = new ArrayList<String>();
+		
+		list.add("JANUARY");
+		list.add("FEBRUARY");
+		list.add("MARCH");
+		list.add("APRIL");
+		list.add("MAY");
+		list.add("JUNE");
+		list.add("JULY");
+		list.add("AUGUST");
+		list.add("SEPTEMBER");
+		list.add("OCTOBER");
+		list.add("NOVEMBER");
+		list.add("DECEMBER");
+		
+		return list;
+	}
+	
+	public static boolean isOneDigit(String value)
+	{
+		if (value.length()==1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
---
+++ src/giantsweetroll/date/DateSelectionPanel.java
@@ -0,0 +1,222 @@
+package giantsweetroll.date;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+import giantsweetroll.gui.swing.Gbm;
+
+public class DateSelectionPanel extends JPanel implements ActionListener
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4573262763411489584L;
+	private JComboBox<String> comboDay, comboMonth, comboYear;
+	private JButton butAuto, butDefault;
+	private GridBagConstraints c;
+	private Date defaultDate;
+	
+	//Constants
+	private final String AUTO = "auto";
+	private final String DEFAULT = "default";
+	
+	//Conditional
+	private boolean enable;
+	
+	//Constructor
+	public DateSelectionPanel(boolean enabled, String autoText, String defaultText, String autoToolTip, String defaultToolTip)
+	{
+		this.enable = enabled;
+		this.init(autoText, defaultText, autoToolTip, defaultToolTip);
+		this.enableComponents(enabled);
+	}
+	
+	//Methods
+	public void enableComponents(boolean enable)
+	{
+		this.comboMonth.setEnabled(enable);
+		this.comboYear.setEnabled(enable);
+		this.comboDay.setEnabled(enable);
+		this.butAuto.setEnabled(enable);
+		this.butDefault.setEnabled(enable);
+	}
+	private final void init(String autoText, String defaultText, String autoToolTip, String defaultToolTip)
+	{
+		//Initialization
+		this.comboYear = new JComboBox<String>(DateManager.getYearRangeStringReversed());
+	//	this.comboYear = new JComboBox<String>(DateManager.getYearRangeString());
+		this.comboMonth = new JComboBox<String>(DateManager.getMonthNameList().toArray(new String[12]));
+		this.comboDay = new JComboBox<String>(DateManager.getMaxDaysString(Byte.parseByte(Integer.toString(this.comboMonth.getSelectedIndex()+1)), Short.parseShort(this.comboYear.getSelectedItem().toString())));
+		this.butAuto = new JButton(autoText);
+		this.butDefault = new JButton("defaultText");
+		this.c = new GridBagConstraints();
+		this.defaultDate = new Date();
+		
+		//Properties
+		this.setLayout(new GridBagLayout());
+		this.setOpaque(false);
+		this.comboYear.addActionListener(this.yearListener);
+		this.comboMonth.addActionListener(this.monthListener);
+		this.comboDay.setEnabled(this.enable);
+		this.butAuto.addActionListener(this);
+		this.butAuto.setActionCommand(this.AUTO);
+		this.butAuto.setToolTipText(autoToolTip);
+		this.butDefault.addActionListener(this);
+		this.butDefault.setActionCommand(this.DEFAULT);
+		this.butDefault.setToolTipText(defaultToolTip);
+		this.comboDay.setBackground(Color.WHITE);
+		this.comboMonth.setBackground(Color.WHITE);
+		this.comboYear.setBackground(Color.WHITE);
+		this.setAsDefaultDataThis();
+		
+		//Add to panel
+		Gbm.goToOrigin(c);
+		c.insets = new Insets(5, 5, 5, 5);
+	//	this.vecDay = new VectorInt(c.gridx, c.gridy);
+		this.add(this.comboDay, c);							//Day
+		Gbm.nextGridColumn(c);
+		this.add(this.comboMonth, c);						//Month
+		Gbm.nextGridColumn(c);
+		this.add(this.comboYear, c);						//Year
+		Gbm.nextGridColumn(c);
+		this.add(this.butDefault, c);
+		Gbm.nextGridColumn(c);
+		this.add(this.butAuto, c);							//Auto Button
+	}
+	
+	private void refreshDayRange()
+	{	
+		this.comboDay.setModel(new DefaultComboBoxModel<String>(DateManager.getMaxDaysString(Byte.parseByte(Integer.toString(this.comboMonth.getSelectedIndex()+1)), 
+																				Short.parseShort(this.comboYear.getSelectedItem().toString()))));
+		
+		this.revalidate();
+		this.repaint();
+	}
+	
+	public void setDate(Date date)
+	{
+		this.comboYear.setSelectedItem(Integer.toString(date.getYear()));
+		this.comboMonth.setSelectedIndex(date.getMonth()-1);
+		this.comboDay.setSelectedIndex(date.getDay()-1);
+	}
+	
+	public String getDay()
+	{
+		return this.comboDay.getSelectedItem().toString();
+	}
+	public String getMonth()			//As String
+	{
+		return this.comboMonth.getSelectedItem().toString();
+	}
+	public int getMonthValue()			//In value form
+	{
+		return this.comboMonth.getSelectedIndex() + 1;
+	}
+	public String getYear()
+	{
+		return this.comboYear.getSelectedItem().toString();
+	}
+	
+	public void setAsDefaultDataThis()
+	{
+		this.setDefaultDate(new Date(Integer.parseInt(this.getDay()), this.getMonthValue(), Integer.parseInt(this.getYear())));
+	}
+	
+	public void setDefaultDate(Date date)
+	{
+		this.defaultDate = date;
+	}
+	
+	public void resetDefault()
+	{
+		try
+		{
+			this.setDate(this.defaultDate);
+		}
+		catch(NullPointerException ex){ex.printStackTrace();}
+	}
+	
+	public void autoSetDate()
+	{
+		this.setDate(new Date());
+	}
+	
+	public boolean sameAsDefault()
+	{
+		Date date = this.getDate();
+		if (this.defaultDate.getYear() == date.getYear() && 
+				this.defaultDate.getMonth() == date.getMonth() && 
+				this.defaultDate.getDay() == date.getDay())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public String getDateAsString()
+	{
+		return this.getDay() + "/" + this.getMonthValue() + "/" + this.getYear();
+	}
+	
+	public Date getDate()
+	{
+		return new Date(Integer.parseInt(this.getDay()), this.getMonthValue(), Integer.parseInt(this.getYear()));
+	}
+	
+	//Overriden Methods
+	@Override
+	public void setEnabled(boolean enable)
+	{
+		super.setEnabled(enable);
+		
+		this.comboDay.setEnabled(enable);
+		this.comboMonth.setEnabled(enable);
+		this.comboYear.setEnabled(enable);
+		this.butAuto.setEnabled(enable);
+		this.butDefault.setEnabled(enable);
+	}
+	
+	//Interfaces
+	private ActionListener yearListener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				refreshDayRange();
+			}
+		};
+			
+	private ActionListener monthListener = new ActionListener()
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			refreshDayRange();
+		}
+	};
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		switch(e.getActionCommand())
+		{
+			case AUTO:
+				this.autoSetDate();
+				break;
+				
+			case DEFAULT:
+				this.resetDefault();
+				break;
+		}
+	}
+}
